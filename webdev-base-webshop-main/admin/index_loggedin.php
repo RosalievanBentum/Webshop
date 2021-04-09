43cc10796d4aa3d72f50f88e22455a75bcd923e1
RosalievanBentum <71488650+RosalievanBentum@users.noreply.github.com>
2021-04-09 08:54:32 +0200

    Add files via upload

---
+++ webdev-base-webshop-main/admin/index_loggedin.php
@@ -0,0 +1,24 @@
+<?php
+    include('core/header.php');
+    include('core/checklogin_admin.php');
+?>
+<h1>Welkom gebruiker <?php echo $_SESSION['Sadmin_id'];?></h1>
+- <a href="logout.php">Uitloggen</a>
+- <a href="?logout=1">Uitloggen met code in indexloggenid</a> <br>
+
+<?php 
+if ( isset($_GET['logout']) && $_GET['logout'] == '1') {
+unset($_SESSION['Sadmin_id']);
+unset($_SESSION['Sadmin_email']);
+header("location:index.php");
+}
+?>
+
+<ul>
+    <li><a href="users/">Gebruikers</a></li>
+    <li><a href="orders/">Bestellingen</a></li>
+    <li><a href="producten">Producten</a></li>
+</ul>
+<?php
+    include('core/footer.php');
+?>
